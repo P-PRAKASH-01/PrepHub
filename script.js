0832bfb26c7d7bea993c432e754e57d7916a3cc1
P-PRAKASH-01 <prakash5361869@gmail.com>
2025-12-24 19:20:29 +0530

    version 1.0

--- script.js
+++ script.js
@@ -1018,7 +1018,7 @@ window.addEventListener('appinstalled', () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then((registration) => {
         console.log('[PWA] Service Worker registered successfully:', registration.scope);
 
